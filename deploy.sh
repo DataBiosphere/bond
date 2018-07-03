94f4388b963092c2c527ba9e9f61e4ab303f80e8
Matt Bemis <mbemis@broadinstitute.org>
2018-07-03 11:00:33 -0400

    deploy updates

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,7 @@ fi
 
 GOOGLE_PROJECT=broad-bond-$ENVIRONMENT
 
+#pull the credentials for the service account
 docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox vault read --format=json "secret/dsde/bond/$ENVIRONMENT/deploy-account.json" | jq .data > deploy_account.json
 
 #activate as a service account that has the App Engine Deployer role
@@ -27,45 +28,46 @@ gcloud auth activate-service-account --key-file=deploy_account.json
 # render the .ini
 docker run -v $PWD:/app \
   -e INPUT_PATH=/app/configs-pre \
-  -e OUT_PATH=/app/output \
+  -e OUT_PATH=/app/ \
   -e VAULT_TOKEN=$VAULT_TOKEN \
   -e ENVIRONMENT=$ENVIRONMENT \
   broadinstitute/dsde-toolbox render-templates.sh
 
-#render the endpoints json
+#build the docker image so we can 
 docker build -f docker/Dockerfile -t databiosphere/bond:deploy .
+
+#render the endpoints json
 docker run -v $PWD/output/config.ini:/app/config.ini \
     -v $PWD/startup.sh:/app/startup.sh \
     -v $PWD/output:/output \
     -e GOOGLE_PROJECT=$GOOGLE_PROJECT \
     databiosphere/bond:deploy /bin/bash -c "python lib/endpoints/endpointscfg.py get_openapi_spec main.BondApi --hostname $GOOGLE_PROJECT.appspot.com; cp linkv1openapi.json /output"
 
-
 #deploy google endpoints
 gcloud -q endpoints services deploy output/linkv1openapi.json --project $GOOGLE_PROJECT
 
+#SERVICE_VERSION in app.yaml needs to match this
+#SERVICE_VERSION=`gcloud endpoints services describe $GOOGLE_PROJECT.appspot.com --format=json --project $GOOGLE_PROJECT | jq .serviceConfig.id` #todo: gcloud returns different response when calling as a service account and google doesn't know why
 SERVICE_VERSION="dummy"
 
-#SERVICE_VERSION in app.yaml needs to match this
-#NOTE: this is the proper way to do this. But for some reason gcloud doesn't output the full details of the service when using a service account.
-#SERVICE_VERSION=`gcloud endpoints services describe $GOOGLE_PROJECT.appspot.com --format=json --project $GOOGLE_PROJECT | jq .serviceConfig.id`
-
-#render app.yaml and config.ini for environment with SERVICE_VERSION and GOOGLE_PROJECT
+#render app.yaml for environment with SERVICE_VERSION and GOOGLE_PROJECT
 docker run -v $PWD:/app \
   -e GOOGLE_PROJ=$GOOGLE_PROJECT \
   -e SERVICE_VERSION=$SERVICE_VERSION \
   -e INPUT_PATH=/app/configs-post \
-  -e OUT_PATH=/app/output \
+  -e OUT_PATH=/app/ \
   -e VAULT_TOKEN=$VAULT_TOKEN \
   -e ENVIRONMENT=$ENVIRONMENT \
   broadinstitute/dsde-toolbox render-templates.sh
 
-cp output/app.yaml .
-cp output/config.ini .
-
+#start the container
 CONTAINER_ID=`docker create databiosphere/bond:deploy`
 
+#copy all of the libs
 docker cp $CONTAINER_ID:/app/lib .
+
+#remove the container
+docker rm $CONTAINER_ID
 
 #deploy the app to the specified project
 gcloud -q app deploy app.yaml --project=$GOOGLE_PROJECT
