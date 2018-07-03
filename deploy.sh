33c35999d13d03ec69fc64e6c6b12a2bb42c6e1e
Matt Bemis <mbemis@broadinstitute.org>
2018-07-03 17:13:06 -0400

    v2

--- deploy.sh
+++ deploy.sh
@@ -22,11 +22,13 @@ GOOGLE_PROJECT=broad-bond-$ENVIRONMENT
 #pull the credentials for the service account
 docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox vault read --format=json "secret/dsde/bond/$ENVIRONMENT/deploy-account.json" | jq .data > deploy_account.json
 
-#activate as a service account that has the App Engine Deployer role
-gcloud auth activate-service-account --key-file=deploy_account.json
-
-#build the docker image so we can 
+#build the docker image so we can deploy
 docker build -f docker/Dockerfile -t databiosphere/bond:deploy .
+
+#activate as a service account that has the App Engine Deployer role
+docker run -v $PWD/startup.sh:/app/startup.sh \
+    -v $PWD/deploy_account.json:/deploy_account.json \
+    databiosphere/bond:deploy /bin/bash -c "gcloud auth activate-service-account --key-file=deploy_account.json"
 
 #render the endpoints json
 docker run -v $PWD/startup.sh:/app/startup.sh \
@@ -35,33 +37,27 @@ docker run -v $PWD/startup.sh:/app/startup.sh \
     databiosphere/bond:deploy /bin/bash -c "python lib/endpoints/endpointscfg.py get_openapi_spec main.BondApi --hostname $GOOGLE_PROJECT.appspot.com; cp linkv1openapi.json /output"
 
 #deploy google endpoints
-gcloud -q endpoints services deploy output/linkv1openapi.json --project $GOOGLE_PROJECT
+docker run -v $PWD/startup.sh:/app/startup.sh \
+    -v $PWD/output:/output \
+    -e GOOGLE_PROJECT=$GOOGLE_PROJECT \
+    databiosphere/bond:deploy /bin/bash -c "gcloud -q endpoints services deploy output/linkv1openapi.json --project $GOOGLE_PROJECT"
 
 #SERVICE_VERSION in app.yaml needs to match this
 #SERVICE_VERSION=`gcloud endpoints services describe $GOOGLE_PROJECT.appspot.com --format=json --project $GOOGLE_PROJECT | jq .serviceConfig.id` #todo: gcloud returns different response when calling as a service account and google doesn't know why
-SERVICE_VERSION=`date +%Y-%m-%d`r0
+SERVICE_VERSION=`date +%Y-%m-%d`r0 #todo: until google fixes the above gcloud command, we will use this. it will work a max of once per day (because it only uses r0 for each date)
 
-#render app.yaml for environment with SERVICE_VERSION and GOOGLE_PROJECT
+#render config.ini and app.yaml for environment with SERVICE_VERSION and GOOGLE_PROJECT
 docker run -v $PWD:/app \
   -e GOOGLE_PROJ=$GOOGLE_PROJECT \
   -e SERVICE_VERSION=$SERVICE_VERSION \
   -e INPUT_PATH=/app \
-  -e OUT_PATH=/app/output \
+  -e OUT_PATH=/app \
   -e VAULT_TOKEN=$VAULT_TOKEN \
   -e ENVIRONMENT=$ENVIRONMENT \
   broadinstitute/dsde-toolbox render-templates.sh
 
-#copy out the rendered configs
-cp -r output/. .
-
-#start the container
-CONTAINER_ID=`docker create databiosphere/bond:deploy`
-
-#copy all of the libs
-docker cp $CONTAINER_ID:/app/lib .
-
-#remove the container
-docker rm $CONTAINER_ID
-
 #deploy the app to the specified project
-gcloud -q app deploy app.yaml --project=$GOOGLE_PROJECT
+docker run -v $PWD/startup.sh:/app/startup.sh \
+    -v $PWD/output:/output \
+    -e GOOGLE_PROJECT=$GOOGLE_PROJECT \
+    databiosphere/bond:deploy /bin/bash -c "gcloud -q app deploy app.yaml --project=$GOOGLE_PROJECT"
