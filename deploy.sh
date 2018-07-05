a291fc08157e11817d99215d725c6cb9627c639e
Jackie Roberti <jacqueline.roberti93@gmail.com>
2018-07-05 11:38:41 -0400

    fix deploy path

--- deploy.sh
+++ deploy.sh
@@ -49,7 +49,7 @@ docker run -v $PWD:/app \
 
 #deploy the app to the specified project
 docker run -v $PWD/startup.sh:/app/startup.sh \
-    -v $PWD/app.yaml:/app.yaml \
+    -v $PWD/app.yaml:/app/app.yaml \
     -v $PWD/deploy_account.json:/deploy_account.json \
     -e GOOGLE_PROJECT=$GOOGLE_PROJECT \
     databiosphere/bond:deploy /bin/bash -c \
