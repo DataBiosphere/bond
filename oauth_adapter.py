cabfed6789c7be93fd5307fce12a88853c656a26
Douglas Voet <dvoet@users.noreply.github.com>
2018-07-27 15:18:51 -0400

    use .well-known/openid-configuration url to figure out token url

--- oauth_adapter.py
+++ oauth_adapter.py
@@ -1,6 +1,11 @@
 from requests.auth import HTTPBasicAuth
 from requests_oauthlib import OAuth2Session
 from requests_toolbelt.adapters import appengine
+from google.appengine.api import memcache
+from google.appengine.api import urlfetch
+import endpoints
+import json
+
 
 # https://toolbelt.readthedocs.io/en/latest/adapters.html#appengineadapter
 appengine.monkeypatch()
@@ -8,12 +13,12 @@ appengine.monkeypatch()
 
 class OauthAdapter:
 
-    def __init__(self, client_id, client_secret, redirect_url, token_url):
+    def __init__(self, client_id, client_secret, open_id_config_url, provider_name):
         self.client_id = client_id
         self.client_secret = client_secret
-        self.redirect_url = redirect_url
-        self.token_url = token_url
+        self.open_id_config_url = open_id_config_url
         self.basic_auth = HTTPBasicAuth(self.client_id, self.client_secret)
+        self.provider_name = provider_name
 
     def exchange_authz_code(self, authz_code, redirect_uri):
         """
@@ -22,10 +27,8 @@ class OauthAdapter:
         :param redirect_uri: redirect_uri that was used to get the token - will use self.redirect_url if None
         :return: A token dict including the access token, refresh token, and token type (amongst other details)
         """
-        if not redirect_uri:
-            redirect_uri = self.redirect_url
         oauth = OAuth2Session(self.client_id, redirect_uri=redirect_uri)
-        return oauth.fetch_token(self.token_url, code=authz_code, auth=self.basic_auth)
+        return oauth.fetch_token(self._get_token_info_url(), code=authz_code, auth=self.basic_auth)
 
     def refresh_access_token(self, refresh_token_str):
         """
@@ -35,4 +38,36 @@ class OauthAdapter:
         """
         token_dict = {'refresh_token': refresh_token_str}
         oauth = OAuth2Session(self.client_id, token=token_dict)
-        return oauth.refresh_token(self.token_url, auth=self.basic_auth)
+        return oauth.refresh_token(self._get_token_info_url(), auth=self.basic_auth)
+
+    def _token_url(self):
+        open_id_config = memcache.get(namespace="OauthAdapter", key=self.provider_name)
+        if open_id_config:
+            return open_id_config["token_endpoint"]
+        else:
+            open_id_config_response = urlfetch.fetch(self.open_id_config_url)
+            if open_id_config_response.status_code/100 != 2:
+                raise endpoints.InternalServerErrorException(
+                    message='open_id_config_url [{}] returned status {}: {}'.format(self.open_id_config_url,
+                                                                                    open_id_config_response.status_code,
+                                                                                    open_id_config_response.content))
+            else:
+                open_id_config = json.loads(open_id_config_response.content)
+                memcache.get(namespace="OauthAdapter", key=self.provider_name, value=open_id_config)
+
+    def _get_open_id_config(self):
+        open_id_config = memcache.get(namespace="OauthAdapter", key=self.provider_name)
+        if not open_id_config:
+            open_id_config_response = urlfetch.fetch(self.open_id_config_url)
+            if open_id_config_response.status_code != 200:
+                raise endpoints.InternalServerErrorException(
+                    message='open_id_config_url [{}] returned status {}: {}'.format(self.open_id_config_url,
+                                                                                    open_id_config_response.status_code,
+                                                                                    open_id_config_response.content))
+            else:
+                open_id_config = json.loads(open_id_config_response.content)
+                memcache.add(namespace="OauthAdapter", key=self.provider_name, value=open_id_config)
+        return open_id_config
+
+    def _get_token_info_url(self):
+        return self._get_open_id_config()["token_endpoint"]
