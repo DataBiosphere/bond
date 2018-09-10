63b5d1391ff9a438aa742a392b9a0a0fafebce32
Greg Polumbo <36171087+gpolumbo-broad@users.noreply.github.com>
2018-09-10 13:18:45 -0400

    Fixing bond deployment script to use a custom entrypoint when using the Bond docker container to perform actions (#51)

--- deploy.sh
+++ deploy.sh
@@ -34,8 +34,9 @@ docker run -v $PWD/startup.sh:/app/startup.sh \
     -v $PWD/output:/output \
     -v $PWD/deploy_account.json:/deploy_account.json \
     -e GOOGLE_PROJECT=$GOOGLE_PROJECT \
-    databiosphere/bond:deploy /bin/bash -c \
-    "gcloud auth activate-service-account --key-file=deploy_account.json; python lib/endpoints/endpointscfg.py get_openapi_spec main.BondApi main.BondStatusApi --hostname $GOOGLE_PROJECT.appspot.com --x-google-api-name; gcloud -q endpoints services deploy linkv1openapi.json statusv1openapi.json --project $GOOGLE_PROJECT"
+    --entrypoint "/bin/bash" \
+    databiosphere/bond:deploy \
+    -c "gcloud auth activate-service-account --key-file=deploy_account.json; python lib/endpoints/endpointscfg.py get_openapi_spec main.BondApi main.BondStatusApi --hostname $GOOGLE_PROJECT.appspot.com --x-google-api-name; gcloud -q endpoints services deploy linkv1openapi.json statusv1openapi.json --project $GOOGLE_PROJECT"
 
 #SERVICE_VERSION in app.yaml needs to match the output of the curl call below
 export BUILD_TMP="${HOME}/deploy-bond-${GIT_BRANCH}"
@@ -61,5 +62,6 @@ docker run -v $PWD/startup.sh:/app/startup.sh \
     -v $PWD/config.ini:/app/config.ini \
     -v $PWD/deploy_account.json:/deploy_account.json \
     -e GOOGLE_PROJECT=$GOOGLE_PROJECT \
-    databiosphere/bond:deploy /bin/bash -c \
-    "gcloud auth activate-service-account --key-file=deploy_account.json; gcloud -q app deploy app.yaml --project=$GOOGLE_PROJECT"
+    --entrypoint "/bin/bash" \
+    databiosphere/bond:deploy \
+    -c "gcloud auth activate-service-account --key-file=deploy_account.json; gcloud -q app deploy app.yaml --project=$GOOGLE_PROJECT"
