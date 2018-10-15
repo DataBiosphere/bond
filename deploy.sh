7f35f7761beda2d05ab141654748c2d719ddedac
Greg Polumbo <36171087+gpolumbo-broad@users.noreply.github.com>
2018-10-15 10:24:11 -0400

    Updating deploy.sh  (#63)
    * GAWB-3855 Updating deploy.sh to use the bond image for the branch to render configs and do gcloud deployments
    
    * GAWB-3855 backwards compatible deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,42 +4,61 @@ set -x
 
 VAULT_TOKEN=$1
 GIT_BRANCH=$2
+TARGET_ENV=$3
 
-#need to get the environment from the branch name
-if [ "$GIT_BRANCH" == "develop" ]; then
-	ENVIRONMENT="dev"
-elif [ "$GIT_BRANCH" == "alpha" ]; then
-        ENVIRONMENT="alpha"
-elif [ "$GIT_BRANCH" == "perf" ]; then
-	ENVIRONMENT="perf"
-elif [ "$GIT_BRANCH" == "staging" ]; then
-	ENVIRONMENT="staging"
-elif [ "$GIT_BRANCH" == "master" ]; then
-	ENVIRONMENT="prod"
-else
-	echo "Unknown Git branch $GIT_BRANCH"
-	exit 1
+set +x
+if [ -z "$TARGET_ENV" ]; then
+    echo "TARGET_ENV argument not supplied; inferring from GIT_BRANCH '$GIT_BRANCH'."
+
+    if [ "$GIT_BRANCH" == "develop" ]; then
+        TARGET_ENV="dev"
+    elif [ "$GIT_BRANCH" == "alpha" ]; then
+        TARGET_ENV="alpha"
+    elif [ "$GIT_BRANCH" == "perf" ]; then
+        TARGET_ENV="perf"
+    elif [ "$GIT_BRANCH" == "staging" ]; then
+        TARGET_ENV="staging"
+    elif [ "$GIT_BRANCH" == "master" ]; then
+        TARGET_ENV="prod"
+    else
+        echo "Git branch '$GIT_BRANCH' is not configured to automatically deploy to a target environment"
+        exit 1
+    fi
 fi
 
-GOOGLE_PROJECT=broad-bond-$ENVIRONMENT
+if [[ "$TARGET_ENV" =~ ^(dev|alpha|perf|staging|prod)$ ]]; then
+    ENVIRONMENT=${TARGET_ENV}
+else
+    echo "Unknown environment: $TARGET_ENV - must be one of [dev, alpha, perf, staging, prod]"
+    exit 1
+fi
+
+echo "Deploying branch '${GIT_BRANCH}' to ${ENVIRONMENT}"
+set -x
+
+GOOGLE_PROJECT=broad-bond-${ENVIRONMENT}
+BOND_IMAGE=quay.io/databiosphere/bond:${GIT_BRANCH}
 
 #pull the credentials for the service account
-docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox vault read --format=json "secret/dsde/bond/$ENVIRONMENT/deploy-account.json" | jq .data > deploy_account.json
+docker run --rm -e VAULT_TOKEN=${VAULT_TOKEN} broadinstitute/dsde-toolbox vault read --format=json "secret/dsde/bond/$ENVIRONMENT/deploy-account.json" | jq .data > deploy_account.json
+
+if [ ! -s deploy_account.json ]; then
+    echo "Failed to create deploy_account.json"
+    exit 1
+fi
 
 #build the docker image so we can deploy
-docker build -f docker/Dockerfile -t databiosphere/bond:deploy .
+docker pull ${BOND_IMAGE}
 
 #render the endpoints json and then deploy it
-docker run -v $PWD/startup.sh:/app/startup.sh \
-    -v $PWD/output:/output \
-    -v $PWD/deploy_account.json:/deploy_account.json \
-    -e GOOGLE_PROJECT=$GOOGLE_PROJECT \
+docker run -v $PWD/deploy_account.json:/app/deploy_account.json \
+    -e GOOGLE_PROJECT=${GOOGLE_PROJECT} \
     --entrypoint "/bin/bash" \
-    databiosphere/bond:deploy \
+    ${BOND_IMAGE} \
     -c "gcloud auth activate-service-account --key-file=deploy_account.json; python lib/endpoints/endpointscfg.py get_openapi_spec main.BondApi main.BondStatusApi --hostname $GOOGLE_PROJECT.appspot.com --x-google-api-name; gcloud -q endpoints services deploy linkv1openapi.json statusv1openapi.json --project $GOOGLE_PROJECT"
 
 #SERVICE_VERSION in app.yaml needs to match the output of the curl call below
-export BUILD_TMP="${HOME}/deploy-bond-${GIT_BRANCH}"
+export BUILD_TMP="${HOME}/deploy-bond-${TARGET_ENV}"
 mkdir -p ${BUILD_TMP}
 export CLOUDSDK_CONFIG=${BUILD_TMP}
 
@@ -48,22 +67,21 @@ SERVICE_VERSION=$(curl --silent --header "Authorization: Bearer `gcloud auth pri
 
 #render config.ini and app.yaml for environment with SERVICE_VERSION and GOOGLE_PROJECT
 docker run -v $PWD:/app \
-  -e GOOGLE_PROJ=$GOOGLE_PROJECT \
-  -e SERVICE_VERSION=$SERVICE_VERSION \
+  -e GOOGLE_PROJ=${GOOGLE_PROJECT} \
+  -e SERVICE_VERSION=${SERVICE_VERSION} \
   -e INPUT_PATH=/app \
   -e OUT_PATH=/app \
-  -e VAULT_TOKEN=$VAULT_TOKEN \
-  -e ENVIRONMENT=$ENVIRONMENT \
+  -e VAULT_TOKEN=${VAULT_TOKEN} \
+  -e ENVIRONMENT=${ENVIRONMENT} \
   -e RUN_CONTEXT=live \
   -e DNS_DOMAIN=NULL \
   broadinstitute/dsde-toolbox render-templates.sh
 
 #deploy the app to the specified project
-docker run -v $PWD/startup.sh:/app/startup.sh \
-    -v $PWD/app.yaml:/app/app.yaml \
+docker run -v $PWD/app.yaml:/app/app.yaml \
     -v $PWD/config.ini:/app/config.ini \
-    -v $PWD/deploy_account.json:/deploy_account.json \
-    -e GOOGLE_PROJECT=$GOOGLE_PROJECT \
+    -v $PWD/deploy_account.json:/app/deploy_account.json \
+    -e GOOGLE_PROJECT=${GOOGLE_PROJECT} \
     --entrypoint "/bin/bash" \
-    databiosphere/bond:deploy \
+    ${BOND_IMAGE} \
     -c "gcloud auth activate-service-account --key-file=deploy_account.json; gcloud -q app deploy app.yaml --project=$GOOGLE_PROJECT"
