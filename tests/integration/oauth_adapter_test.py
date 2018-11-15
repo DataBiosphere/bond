c728d87c0a1a6a388299657aca521fa8b84d2d02
Greg Polumbo <gpolumbo@broadinstitute.org>
2018-11-15 11:16:26 -0500

    GAWB-3807 Refactoring oauth_adapter_test.py so that I know if the mock fence is conforming to the contract

--- tests/integration/oauth_adapter_test.py
+++ tests/integration/oauth_adapter_test.py
@@ -1,6 +1,8 @@
 import ConfigParser
 import unittest
 import endpoints
+import re
+import urllib
 
 from google.appengine.ext import testbed
 from oauth_adapter import OauthAdapter
@@ -10,40 +12,70 @@ class OauthAdapterTestCase(unittest.TestCase):
 
     def setUp(self):
         super(OauthAdapterTestCase, self).setUp()
+
         self.tb = testbed.Testbed()
         self.tb.activate()
         self.tb.init_memcache_stub()
         self.tb.init_urlfetch_stub()
+
+        self.url_prefix_regex = "^http(s)?:\/\/[a-z0-9_\-]+(\.[a-z0-9_\-]+)+"
         config = ConfigParser.ConfigParser()
         config.read("config.ini")
-        self.provider = "fence"
-        self.client_id = config.get(self.provider, 'CLIENT_ID')
-        self.client_secret = config.get(self.provider, 'CLIENT_SECRET')
-        self.open_id_config_url = config.get(self.provider, 'OPEN_ID_CONFIG_URL')
-        self.oauth_adapter = OauthAdapter(self.client_id, self.client_secret, self.open_id_config_url, self.provider)
+        self.oauth_adapters = {}
+        for section in config.sections():
+            if section != "sam":
+                client_id = config.get(section, 'CLIENT_ID')
+                client_secret = config.get(section, 'CLIENT_SECRET')
+                open_id_config_url = config.get(section, 'OPEN_ID_CONFIG_URL')
+                self.oauth_adapters[section] = OauthAdapter(client_id, client_secret, open_id_config_url, section)
 
     def tearDown(self):
         self.tb.deactivate()
         super(OauthAdapterTestCase, self).tearDown()
 
+    @staticmethod
+    def param_regex(key, value):
+        return "[\?&]" + re.escape(key) + "=" + re.escape(value) + "[&]?"
+
     def test_get_open_id_config(self):
-        open_id_config = self.oauth_adapter._get_open_id_config()
-        self.assertIsNotNone(open_id_config)
+        for provider, oauth_adapter in self.oauth_adapters.iteritems():
+            open_id_config = oauth_adapter._get_open_id_config()
+            self.assertIsNotNone(open_id_config, "Expected open_id_config for " + provider + " to not be None")
 
     def test_get_open_id_config_value(self):
-        key = "scopes_supported"
-        self.assertTrue(len(self.oauth_adapter._get_open_id_config_value(key)),
-                        "Expected key \"" + key + "\" to be a non-empty array")
+        for provider, oauth_adapter in self.oauth_adapters.iteritems():
+            key = "scopes_supported"
+            self.assertTrue(len(oauth_adapter._get_open_id_config_value(key)),
+                            "Expected key \"" + key + "\" to be a non-empty array for provider " + provider)
 
     def test_get_open_id_config_value_missing(self):
-        with self.assertRaises(endpoints.InternalServerErrorException):
-            self.oauth_adapter._get_open_id_config_value("something-that-does-not-exist")
+        for provider, oauth_adapter in self.oauth_adapters.iteritems():
+            with self.assertRaises(endpoints.InternalServerErrorException):
+                oauth_adapter._get_open_id_config_value("something-that-does-not-exist")
 
     def test_get_open_id_config_value_missing_no_exception(self):
-        self.assertIsNone(self.oauth_adapter._get_open_id_config_value("something-that-does-not-exist", False))
+        for provider, oauth_adapter in self.oauth_adapters.iteritems():
+            self.assertIsNone(oauth_adapter._get_open_id_config_value("something-that-does-not-exist", False))
 
     def test_get_token_info_url(self):
-        self.assertRegexpMatches(self.oauth_adapter._get_token_info_url(), "^http(s)?:\/\/[a-z0-9_\-]+(\.[a-z0-9_\-]+)+")
+        for provider, oauth_adapter in self.oauth_adapters.iteritems():
+            token_info_url = oauth_adapter._get_token_info_url()
+            self.assertRegexpMatches(token_info_url, self.url_prefix_regex, "Token Info URL for provider " + provider)
 
     def test_get_revoke_url(self):
-        self.assertRegexpMatches(self.oauth_adapter._get_revoke_url(), "^http(s)?:\/\/.+?revoke")
+        url_regex = "^http(s)?:\/\/.+?revoke"
+        for provider, oauth_adapter in self.oauth_adapters.iteritems():
+            self.assertRegexpMatches(oauth_adapter._get_revoke_url(), url_regex, "Revoke URL for provider " + provider)
+
+    def test_build_authz_url(self):
+        scopes = ["foo", "bar"]
+        redirect_uri = "http://something.something/"
+        state = "abc123"
+        for provider, oauth_adapter in self.oauth_adapters.iteritems():
+            authz_url = oauth_adapter.build_authz_url(scopes, redirect_uri, state)
+            msg = "For provider: " + provider
+            self.assertRegexpMatches(authz_url, self.url_prefix_regex, msg)
+            self.assertRegexpMatches(authz_url, self.param_regex("response_type", "code"), msg)
+            self.assertRegexpMatches(authz_url, self.param_regex("scope", "+".join(scopes)), msg)
+            self.assertRegexpMatches(authz_url, self.param_regex("redirect_uri", urllib.quote_plus(redirect_uri)), msg)
+            self.assertRegexpMatches(authz_url, self.param_regex("state", state), msg)
