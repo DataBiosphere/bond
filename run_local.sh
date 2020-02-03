d0590a63323bde0b38516c2a2567d78914cf9e1f
wchambers <1499870+wchamber@users.noreply.github.com>
2020-02-03 10:36:18 -0500

    Check the DATASTORE_EMULATOR_HOST environment variable to decide how to create datastore client credentials. Add an argument to run_local.sh for selecting the host.

--- run_local.sh
+++ run_local.sh
@@ -4,4 +4,6 @@ export FLASK_APP=main.py
 # Configure the app to talk to the Datastore Emulator.
 export DATASTORE_EMULATOR_HOST=0.0.0.0:8432
 
-flask run --debugger
+FLASK_HOST="${1:-localhost}"
+
+flask run --debugger -p 8080 -h $FLASK_HOST
