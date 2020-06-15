65bf3f7c5d48c9dff6d06ab1d5c22a98fd54f27f
Zybjana Bedo <zbedo@broadinstitute.org>
2020-06-15 16:59:48 -0400

    [burp] Check proxy configuration

--- security/burp_scan.sh
+++ security/burp_scan.sh
@@ -6,6 +6,10 @@ set -eu
 # authenticated with container registry using burp_start.sh)
 IMAGE="$1"
 
+#Check proxy config
+python3 BroadBurpScanner.py http://localhost --action proxy-config
+
+
 # Scan collected traffic and report results (optional)
 docker run --rm -it --entrypoint /automation/BroadBurpScanner.py "${IMAGE}" \
   http://localhost --action scan
