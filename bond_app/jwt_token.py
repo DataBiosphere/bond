e801220d2bd39f28fec7473dfb073535ada9ecfd
Greg Polumbo <36171087+gpolumbo-broad@users.noreply.github.com>
2021-05-07 16:38:54 -0400

    CA-1341 Updating the dpath lib and adding dpath option to ALLOW_EMPTY_STRING_KEYS (#173)

--- bond_app/jwt_token.py
+++ bond_app/jwt_token.py
@@ -1,6 +1,7 @@
 import jwt
 from datetime import datetime
 import dpath.util
+import dpath.options
 
 
 class JwtToken:
@@ -11,5 +12,7 @@ class JwtToken:
         :param user_name_path_expr: forward slash delimited path to user name within JWT
         """
         self.raw_dict = jwt.decode(encoded_str, verify=False)
+        # See https://broadworkbench.atlassian.net/browse/CA-1341, needed for dpath to work when dict has "" keys
+        dpath.options.ALLOW_EMPTY_STRING_KEYS = True
         self.username = dpath.util.get(self.raw_dict, user_name_path_expr)
         self.issued_at = datetime.fromtimestamp(self.raw_dict.get('iat'))
--- tests/unit/jwt_token_test.py
+++ tests/unit/jwt_token_test.py
@@ -1,4 +1,6 @@
 import unittest
+
+import dpath.exceptions
 import jwt
 from bond_app.jwt_token import JwtToken
 from datetime import datetime
@@ -6,11 +8,22 @@ from datetime import datetime
 
 class JwtTokenTestCase(unittest.TestCase):
 
-    def test_init(self):
-        name = "Bob McBob"
-        issued_at = 1528896868
-        data = {"context": {"user": {"name": name}}, 'iat': issued_at}
-        encoded = jwt.encode(data, 'secret', 'HS256')
-        jwt_token = JwtToken(encoded, "/context/user/name")
-        self.assertEqual(name, jwt_token.username)
-        self.assertEqual(datetime.fromtimestamp(issued_at), jwt_token.issued_at)
+    def setUp(self):
+        self.name = "Bob McBob"
+        self.issued_at = 1528896868
+        self.data = {"context": {"user": {"name": self.name}}, 'iat': self.issued_at}
+
+    def encoded(self):
+        return jwt.encode(self.data, 'secret', 'HS256')
+
+    def jwt_token(self):
+        return JwtToken(self.encoded(), "/context/user/name")
+
+    def test_constructor(self):
+        bond_jwt_token = self.jwt_token()
+        self.assertEqual(self.name, bond_jwt_token.username)
+        self.assertEqual(datetime.fromtimestamp(self.issued_at), bond_jwt_token.issued_at)
+
+    def test_jwt_with_empty_key(self):
+        self.data[''] = 'dict entry with empty key'
+        self.test_constructor()
